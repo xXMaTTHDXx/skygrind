e19ff50bdf1a8db8a1250221f6a42be4c5aa31ad
Matthew Cameron <imatthd01@gmail.com>
2017-04-14 13:10:41 -0600

    Hunger Fix

--- hub/src/main/java/net/skygrind/hub/Hub.java
+++ hub/src/main/java/net/skygrind/hub/Hub.java
@@ -1,11 +1,16 @@
 package net.skygrind.hub;
 
 import net.skygrind.core.SkyPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * Created by Matt on 2017-02-11.
  */
-public class Hub extends SkyPlugin {
+public class Hub extends SkyPlugin implements Listener {
 
     @Override
     protected void onModuleEnable() throws Exception {
@@ -16,5 +21,18 @@ public class Hub extends SkyPlugin {
     @Override
     protected void onModuleDisable() throws Exception {
 
+    }
+
+    @EventHandler
+    public void loseHunger(FoodLevelChangeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        player.setHealth(player.getMaxHealth());
+        player.setFoodLevel(20);
     }
 }
